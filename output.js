e3084dd21f5c987230f81d3ce64c7336db66e8d8
sunrui <icarux@126.com>
2021-11-14 10:53:48 +0800

    a comment

--- output.js
+++ output.js
@@ -1,7 +1,9 @@
 function output() {
-    document.write("<hr /><font size=\"5\" face=\"Menlo\">");
+    document.write("<hr /><span style='font-family:Menlo; font-size:larger'>");
     for (let i in arguments) {
         document.write(arguments[i], " ");
     }
-    document.write("</font>");
+    document.write("</span>");
+    msg = 1234;
 }
+output(msg);
